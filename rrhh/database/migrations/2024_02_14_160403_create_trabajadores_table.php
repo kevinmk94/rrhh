ed16841fbbf7fb2128abcd3d31d53c0a51ca60fc
kevinmk94 <kevinmk94@hotmail.com>
2024-02-14 16:51:07 -0500

    empiezo el crud

---
+++ rrhh/database/migrations/2024_02_14_160403_create_trabajadores_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trabajadores', function (Blueprint $table) {
+            $table->id();
+            $table->string('dni');
+            $table->string('nombres');
+            $table->string('apellido_paterno');
+            $table->string('apellido_materno');
+            $table->date('fecha_nacimiento'); 
+            $table->string('edad');
+            $table->string('cargo');
+            $table->string('correo_electronico');
+            $table->string('direccion');
+            $table->string('profesion');
+            $table->string('colegiatura');
+            $table->date('fecha_ingreso'); 
+            $table->date('fecha_baja');
+            $table->string('estado_trabajor'); 
+            $table->string('estado_plataforma');
+            $table->date('fecha_modificacion');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trabajadores');
+    }
+};
--- rrhh/routes/web.php
+++ rrhh/routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\admin\TrabajadorController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -20,7 +20,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::resource('trabajadores',TrabajadorController::class);
