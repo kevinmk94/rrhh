daa6ac8e0a37bab879e743d7dfd05cbebadc162e
kevinmk94 <kevinmk94@hotmail.com>
2024-03-05 17:09:29 -0500

    LOCOS

--- rrhh/resources/views/admin/home.blade.php
+++ rrhh/resources/views/admin/home.blade.php
@@ -15,8 +15,8 @@
                             <div class="card-header border-0 pt-5">
                                 <div class="card-title">
                                     <div class="card-label">
-                                        <div class="font-weight-bolder">Weekly Sales Stats</div>
-                                        <div class="font-size-sm text-muted mt-2">890,344 Sales</div>
+                                        <div class="font-weight-bolder">Afectuacion Presupuestal</div>
+                                        <div class="font-size-sm text-muted mt-2">890,344 Soles - Ultimo mes Devenge</div>
                                     </div>
                                 </div>
                                 <div class="card-toolbar">
@@ -109,15 +109,14 @@
                                         <div class="d-flex align-items-center mr-2">
                                             <div class="symbol symbol-50 symbol-light mr-3 flex-shrink-0">
                                                 <div class="symbol-label">
-                                                    <img src="assets/media/svg/misc/006-plurk.svg" alt="" class="h-50" />
+                                                    <img src="{{ url('') }}/demo2/dist/assets/media/svg/misc/006-plurk.svg" alt="" class="h-50" />
                                                 </div>
                                             </div>
                                             <div>
-                                                <a href="#" class="font-size-h6 text-dark-75 text-hover-primary font-weight-bolder">Top Authors</a>
-                                                <div class="font-size-sm text-muted font-weight-bold mt-1">Ricky Hunt, Sandra Trepp</div>
+                                                <a href="#" class="font-size-h6 text-dark-75 text-hover-primary font-weight-bolder">Reg. 276</a>
                                             </div>
                                         </div>
-                                        <div class="label label-light label-inline font-weight-bold text-dark-50 py-4 px-3 font-size-base">+105$</div>
+                                        <div class="label label-light label-inline font-weight-bold text-dark-50 py-4 px-3 font-size-base">S/. 120 000</div>
                                     </div>
                                     <!--end::Item-->
                                     <!--begin::Item-->
@@ -125,15 +124,15 @@
                                         <div class="d-flex align-items-center mr-2">
                                             <div class="symbol symbol-50 symbol-light mr-3 flex-shrink-0">
                                                 <div class="symbol-label">
-                                                    <img src="assets/media/svg/misc/015-telegram.svg" alt="" class="h-50" />
+                                                    <img src="{{ url('') }}/demo2/dist/assets/media/svg/misc/015-telegram.svg" alt="" class="h-50" />
                                                 </div>
                                             </div>
                                             <div>
-                                                <a href="#" class="font-size-h6 text-dark-75 text-hover-primary font-weight-bolder">Bestsellers</a>
-                                                <div class="font-size-sm text-muted font-weight-bold mt-1">Pitstop Email Marketing</div>
+                                                <a href="#" class="font-size-h6 text-dark-75 text-hover-primary font-weight-bolder">Reg. 728</a>
+                                                {{-- <div class="font-size-sm text-muted font-weight-bold mt-1">Pitstop Email Marketing</div> --}}
                                             </div>
                                         </div>
-                                        <div class="label label-light label-inline font-weight-bold text-dark-50 py-4 px-3 font-size-base">+60$</div>
+                                        <div class="label label-light label-inline font-weight-bold text-dark-50 py-4 px-3 font-size-base">S/. 120 000</div>
                                     </div>
                                     <!--end::Item-->
                                     <!--begin::Item-->
@@ -141,15 +140,15 @@
                                         <div class="d-flex align-items-center mr-2">
                                             <div class="symbol symbol-50 symbol-light mr-3 flex-shrink-0">
                                                 <div class="symbol-label">
-                                                    <img src="assets/media/svg/misc/003-puzzle.svg" alt="" class="h-50" />
+                                                    <img src="{{ url('') }}/demo2/dist/assets/media/svg/misc/003-puzzle.svg" alt="" class="h-50" />
                                                 </div>
                                             </div>
                                             <div>
-                                                <a href="#" class="font-size-h6 text-dark-75 text-hover-primary font-weight-bolder">Top Engagement</a>
-                                                <div class="font-size-sm text-muted font-weight-bold mt-1">KT.com solution provider</div>
+                                                <a href="#" class="font-size-h6 text-dark-75 text-hover-primary font-weight-bolder">Reg. 1025</a>
+                                                {{-- <div class="font-size-sm text-muted font-weight-bold mt-1">KT.com solution provider</div> --}}
                                             </div>
                                         </div>
-                                        <div class="label label-light label-inline font-weight-bold text-dark-50 py-4 px-3 font-size-base">+75$</div>
+                                        <div class="label label-light label-inline font-weight-bold text-dark-50 py-4 px-3 font-size-base">S/. 120 000</div>
                                     </div>
                                     <!--end::Item-->
                                 </div>
@@ -163,11 +162,11 @@
                         <div class="row">
                             <div class="col-xl-3">
                                 <!--begin::Tiles Widget 3-->
-                                <div class="card card-custom bgi-no-repeat bgi-no-repeat bgi-size-cover gutter-b" style="height: 150px; background-image: url(assets/media/bg/bg-9.jpg)">
+                                <div class="card card-custom bgi-no-repeat bgi-no-repeat bgi-size-cover gutter-b" style="height: 150px; background-image: url({{ url('') }}/demo2/dist/assets/media/bg/bg-9.jpg)">
                                     <!--begin::Body-->
                                     <div class="card-body d-flex flex-column">
                                         <!--begin::Title-->
-                                        <a href="#" class="text-dark-75 text-hover-primary font-weight-bolder font-size-h3">Properties</a>
+                                        <a href="#" class="text-dark-75 text-hover-primary font-weight-bolder font-size-h3">Año 2024</a>
                                         <!--end::Title-->
                                     </div>
                                     <!--end::Body-->
@@ -180,10 +179,10 @@
                                     <!--begin::Body-->
                                     <div class="card-body d-flex align-items-center justify-content-between flex-wrap">
                                         <div class="mr-2">
-                                            <h3 class="font-weight-bolder">Create CRM Reports</h3>
-                                            <div class="text-dark-50 font-size-lg mt-2">Generate the latest CRM Report</div>
+                                            <h3 class="font-weight-bolder">Mantenimiento de Sistema</h3>
+                                            <div class="text-dark-50 font-size-lg mt-2">Modificacion de planillas  </div>
                                         </div>
-                                        <a href="#" class="btn btn-primary font-weight-bold py-3 px-6">Start Now</a>
+                                        <a href="#" class="btn btn-primary font-weight-bold py-3 px-6">Modificar</a>
                                     </div>
                                     <!--end::Body-->
                                 </div>
@@ -193,13 +192,13 @@
                         <div class="row">
                             <div class="col-xl-6">
                                 <!--begin::Tiles Widget 13-->
-                                <div class="card card-custom bgi-no-repeat gutter-b" style="height: 175px; background-color: #663259; background-position: calc(100% + 0.5rem) 100%; background-size: 100% auto; background-image: url(assets/media/svg/patterns/taieri.svg)">
+                                <div class="card card-custom bgi-no-repeat gutter-b" style="height: 175px; background-color: #663259; background-position: calc(100% + 0.5rem) 100%; background-size: 100% auto; background-image: url({{ url('') }}/demo2/dist/assets/media/svg/patterns/taieri.svg)">
                                     <!--begin::Body-->
                                     <div class="card-body d-flex align-items-center">
                                         <div>
-                                            <h3 class="text-white font-weight-bolder line-height-lg mb-5">Create SaaS
-                                            <br />Based Reports</h3>
-                                            <a href='#' class="btn btn-success font-weight-bold px-6 py-3">Create Report</a>
+                                            <h3 class="text-white font-weight-bolder line-height-lg mb-5">Registrar Nuevos
+                                            <br />Trabajadores</h3>
+                                            <a href='#' class="btn btn-success font-weight-bold px-6 py-3">Registrar</a>
                                         </div>
                                     </div>
                                     <!--end::Body-->
@@ -221,8 +220,8 @@
                                                     </svg>
                                                     <!--end::Svg Icon-->
                                                 </span>
-                                                <div class="text-inverse-primary font-weight-bolder font-size-h2 mt-3">790</div>
-                                                <a href="#" class="text-inverse-primary font-weight-bold font-size-lg mt-1">New Products</a>
+                                                <div class="text-inverse-primary font-weight-bolder font-size-h2 mt-3"><a href="#" class="text-inverse-primary font-weight-bolder font-size-h2 mt-3">23</a></div>
+                                                <a href="#" class="text-inverse-primary font-weight-bold font-size-lg mt-1">Cargos</a>
                                             </div>
                                         </div>
                                         <!--end::Tiles Widget 11-->
@@ -232,6 +231,7 @@
                                         <div class="card card-custom gutter-b" style="height: 150px">
                                             <div class="card-body">
                                                 <span class="svg-icon svg-icon-3x svg-icon-success">
+                                                    <a href="#">
                                                     <!--begin::Svg Icon | path:assets/media/svg/icons/Communication/Group.svg-->
                                                     <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
                                                         <g stroke="none" stroke-width="1" fill="none" fill-rule="evenodd">
@@ -240,10 +240,11 @@
                                                             <path d="M17.6011961,15.0006174 C21.0077043,15.0378534 23.7891749,16.7601418 23.9984937,20.4 C24.0069246,20.5466056 23.9984937,21 23.4559499,21 L19.6,21 C19.6,18.7490654 18.8562935,16.6718327 17.6011961,15.0006174 Z M0.00065168429,20.1992055 C0.388258525,15.4265159 4.26191235,13 8.98334134,13 C13.7712164,13 17.7048837,15.2931929 17.9979143,20.2 C18.0095879,20.3954741 17.9979143,21 17.2466999,21 C13.541124,21 8.03472472,21 0.727502227,21 C0.476712155,21 -0.0204617505,20.45918 0.00065168429,20.1992055 Z" fill="#000000" fill-rule="nonzero" />
                                                         </g>
                                                     </svg>
+                                                    </a>
                                                     <!--end::Svg Icon-->
                                                 </span>
-                                                <div class="text-dark font-weight-bolder font-size-h2 mt-3">8,600</div>
-                                                <a href="#" class="text-muted text-hover-primary font-weight-bold font-size-lg mt-1">New Customers</a>
+                                                <div class="text-dark font-weight-bolder font-size-h2 mt-3"><a href="#" class="text-dark font-weight-bolder font-size-h2 mt-3">65</a></div>
+                                                <a href="#" class="text-muted text-hover-primary font-weight-bold font-size-lg mt-1">Trabajadores</a>
                                             </div>
                                         </div>
                                         <!--end::Tiles Widget 12-->
@@ -252,14 +253,14 @@
                             </div>
                             <div class="col-xl-6">
                                 <!--begin::Mixed Widget 14-->
-                                <div class="card card-custom bgi-no-repeat bgi-size-cover gutter-b card-stretch" style="background-image: url(assets/media/stock-600x600/img-16.jpg)">
+                                <div class="card card-custom bgi-no-repeat bgi-size-cover gutter-b card-stretch" style="background-image: url({{ url('') }}/demo2/dist/assets/media/stock-600x600/img-16.jpg)">
                                     <!--begin::Body-->
                                     <div class="card-body d-flex flex-column align-items-start justify-content-start">
                                         <div class="p-1 flex-grow-1">
-                                            <h3 class="text-white font-weight-bolder line-height-lg mb-5">Create Reports
-                                            <br />With App</h3>
+                                            <h3 class="text-white font-weight-bolder line-height-lg mb-5">Generar Planilla
+                                            <br />de trabajadores</h3>
                                         </div>
-                                        <a href='#' class="btn btn-link btn-link-warning font-weight-bold">Create Report
+                                        <a href='#' class="btn btn-link btn-link-warning font-weight-bold">Crear Planilla
                                         <span class="svg-icon svg-icon-lg svg-icon-warning">
                                             <!--begin::Svg Icon | path:assets/media/svg/icons/Navigation/Arrow-right.svg-->
                                             <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="24px" height="24px" viewBox="0 0 24 24" version="1.1">
